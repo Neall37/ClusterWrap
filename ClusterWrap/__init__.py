e5190c34a9af41b34d8ae88cf632cd6ce1c356ac
Sijun Wang <143745468+Neall37@users.noreply.github.com>
2024-08-05 15:30:18 -0400

    Update __init__.py

--- ClusterWrap/__init__.py
+++ ClusterWrap/__init__.py
@@ -1,11 +1,19 @@
 from shutil import which
 import os
-from .clusters import janelia_lsf_cluster, local_cluster
+from .clusters import slurm_cluster, local_cluster
+from .clusters_lsf import janelia_lsf_cluster
 
 cluster = local_cluster
 
 if which('bsub') is not None:
     if os.system('bsub -V') != 32512:
         cluster = janelia_lsf_cluster
-
+        
+if which('srun') is not None:
+    # Run 'srun --version' to verify it works correctly
+    exit_status = os.system('srun --version')
+    
+    # Check if the command executed successfully
+    if os.WEXITSTATUS(exit_status) == 0:
+        cluster = slurm_cluster
 
